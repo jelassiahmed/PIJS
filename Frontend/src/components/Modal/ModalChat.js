b818f6de020db099a9295656d38b1ea95c586204
jelassiahmed <ahmed.jelassi@esprit.tn>
2022-05-10 17:04:41 +0100

    Full project

---
+++ Frontend/src/components/Modal/ModalChat.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import ChatBox from '../../components/Message/index';
+
+const style = {
+  // position: 'fixed',
+  // top: '50%',
+  // left: '50%',
+  // transform: 'translate(-50%, -50%)',
+  // width: 400,
+  // bgcolor: 'background.paper',
+  // border: '2px solid #000',
+  // boxShadow: 24,
+  // p: 4,
+};
+
+export default function ModalChat(props) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [onOFF, setOnOFF] = React.useState(true);
+  React.useEffect(() => {
+    if (props.onOFF !=='Open') {
+      setOnOFF(true);
+    } else {
+      setOnOFF(false);
+    }
+  }, [props.onOFF]);
+  return (
+    <div>
+      <Button onClick={handleOpen} disabled={onOFF}>Open Chat</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <ChatBox refresh={props.refresh} userName={props.userName} complId={props.complId} msgs={props.msgs} close={handleClose}/>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
---
+++ Frontend/src/views/HomePage.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Navbar from '../components/Navbars/IndexNavbar';
+import Carousel from '../components/HomePage/Carousel';
+import Stats from '../components/HomePage/Stats';
+import Footer from '../components/Footers/Footer';
+import CarouselProducts from '../components/Product/CarouselProducts';
+import 'flowbite';
+import Contact from './contactUs/Contact';
+
+export default function HomePage() {
+
+  return (
+    <>
+      <Navbar />
+      <Carousel />
+      <div className="flex flex-col items-center justify-center mt-5">
+        <h1 className="text-3xl font-bold text-center text-white">
+          Welcome to the best platform Baazar!
+        </h1>
+        <h2 className="text-xl font-bold text-center text-white-50">
+          Find the best products in the world!
+        </h2>
+      </div>
+      <br />
+      <CarouselProducts />
+      <br />
+      <div class="carousel w-full">
+        <div id="item1" class="carousel-item w-full">
+          <img src="https://media.mytek.tn/media/webp_image/wysiwyg/banner/Avrl22/pc-portable-gamer-msi-1290-252px-2.webp" class="w-full" />
+        </div>
+      </div>
+      <br />
+      <Stats />
+      <br />
+      <br />
+
+      <br />
+      <Contact show={false} />
+      <Footer />
+    </>
+  )
+}
